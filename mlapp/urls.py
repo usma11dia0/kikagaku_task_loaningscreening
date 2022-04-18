cd917aaf069775964ef57814896fd5c114132e19
usma11dia0 <usma11dia0@gmail.com>
2022-04-18 22:05:09 +0900

    Create_DB_and_template

--- mlapp/urls.py
+++ mlapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('input_form/', views.input_form, name='mlapp/input_form'),
+    path('result/', views.result, name='result') # 追加
 ]
--- mlapp/views.py
+++ mlapp/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import InputForm
+import joblib
+import numpy as np
+from .models import Customer
+
+# モデルの読み込み
+loaded_model = joblib.load('model/ml_model.pkl')
+
 
 def index(request):
     return render(request, 'mlapp/index.html')
+
+
+def input_form(request):
+# 下記の様に編集
+    if request.method == "POST": # Formの入力があった時、
+        form = InputForm(request.POST) # 入力データを取得する。
+        if form.is_valid(): # Formの記載の検証を行い、
+            form.save() # 問題なければ、入力データを保存
+            return redirect('result') # 保存後遷移するページの指定
+    else:
+        form = InputForm()
+        return render(request, 'mlapp/input_form.html', {'form':form})
+
+
+def result(request):
+    # 最新の登録者のデータを取得
+    data = Customer.objects.order_by('id').reverse().values_list('limit_balance', 'education', 'marriage', 'age') # 学習させたカラムの順番
+
+    # 推論の実行
+    x = np.array([data[0]])
+    y = loaded_model.predict(x)
+    y_proba = loaded_model.predict_proba(x)
+    y_proba = y_proba * 100
+    y, y_proba = y[0], y_proba[0] 
+
+    # 推論結果を保存
+    customer = Customer.objects.order_by('id').reverse()[0] # Customerの切り出し
+    customer.proba = y_proba[y]
+    customer.result = y
+    customer.save()
+
+    return render(request, 'mlapp/result.html', {'y':y, 'y_proba':y_proba}) # 推論結果をHTMLに渡す
